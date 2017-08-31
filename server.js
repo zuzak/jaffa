5dd5bdd62683d2e154702c75cd245cba6a67e93e
Douglas Gardner <douglas@chippy.ch>
2017-08-31 21:45:22 +0100

    Strip out session stuff

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var path = require('path') // core
 var sass = require('node-sass-middleware')
 var app = module.exports = express()
 
-var session = require('express-session')
-var JsonSession = require('express-session-json')(session) // TODO something better
 
 app.set('view engine', 'pug')
 app.set('json spaces', 2)
@@ -21,14 +19,6 @@ app.use(sass({
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', express.static(path.join(__dirname, 'node_modules', 'govuk_frontend_toolkit', 'images')))
 
-app.use(require('cookie-parser')())
-app.use(require('body-parser')())
-app.use(session({
-  secret: 'jkshdjakhsjdhajskdhjsakk',
-  resave: false,
-  saveUninitialized: false,
-  store: new JsonSession()
-}))
 
 require('./routes')
 
