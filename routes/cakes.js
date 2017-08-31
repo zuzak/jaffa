96919433db5ac8ada3db4edc050409d01467ecdb
Douglas Gardner <douglas@chippy.ch>
2017-08-31 23:03:51 +0100

    Add cakes

--- routes/cakes.js
+++ routes/cakes.js
@@ -1,6 +1,11 @@
 var app = require('../server.js')
-app.get('/sample/:sample', function (req, res) {
-  res.render('sample.pug', {sample: req.params.sample})
+var Sample = require('../models/samples')
+app.get('/sample/:sample', function (req, res, next) {
+  Sample.find({sampleIdentifier: req.params.sample}, function (err, sample) {
+    if (err) return next(err)
+    res.render('sample.pug', {sample: sample[0]})
+  })
+
 })
 app.get('/results', function (req, res) {
   res.status(501).render('placeholder.pug')
