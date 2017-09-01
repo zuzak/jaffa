a05135c52fd8350bced27d6798482c0fac8ba01b
Douglas Gardner <douglas@chippy.ch>
2017-09-01 01:00:16 +0100

    +

--- routes/misc.js
+++ routes/misc.js
@@ -2,29 +2,21 @@ var app = require('..')
 var exec = require('child_process').exec
 var fs = require('fs')
 
-function rollFortune () {
+
+// load a fortune :)
+app.get('/', function (req, res, next) {
   exec('fortune -s -n60', function (err, stdout, stderr) {
     if (err) {
       console.log(err)
       stdout = 'Helo'
     }
     try {
-      fs.writeFileSync('data/fortune.txt', stdout)
+      res.locals.fortune = stdout
+      next()
     } catch (e) {
       if (e.code === 'ENOENT') {
         fs.mkdirSync('data')
       }
     }
   })
-}
-app.get('/', function (req, res) {
-  rollFortune()
-  try {
-    res.render('home.pug', {req, fortune: fs.readFileSync('data/fortune.txt')})
-  } catch (e) {
-    if (e.code === 'ENOENT') {
-      return res.render('home.pug', {req})
-    }
-    throw e
-  }
 })
