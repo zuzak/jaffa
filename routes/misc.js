1b654405222f7c1cb5405227f9c928f6725115d6
Douglas Gardner <douglas@chippy.ch>
2017-12-08 22:41:28 +0000

    Fix google analytics

--- routes/misc.js
+++ routes/misc.js
@@ -2,6 +2,12 @@ var app = require('..')
 var exec = require('child_process').exec
 var fs = require('fs')
 
+app.use(function (req, res, next) {
+    res.locals.dnt = req.get('DNT')
+    res.locals.user = req.user
+    next()
+})
+
 // load a fortune :)
 app.get('/', function (req, res, next) {
   exec('fortune -s -n60', function (err, stdout, stderr) {
@@ -19,3 +25,5 @@ app.get('/', function (req, res, next) {
     }
   })
 })
+
+
